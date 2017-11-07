f3dce6f98cf074a66706472887e41d6e23f345be
benc9000 <33312669+benc9000@users.noreply.github.com>
2017-11-07 08:22:36 +0000

    Update benc9000.py

--- benc9000.py
+++ benc9000.py
@@ -15,5 +15,5 @@ print "hmm"
 sleep(4)
 print "FIFTY POUNDS EACH, HOW STUPID!"
 print "in the theme park"
-
+print "this is boring, why did we pay 50 pounds to get in. this is the most expensive and boring theme park in the world!
 
